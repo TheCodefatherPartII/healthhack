03f4dfd6169f2db0524841bee7ea0c36621be7a8
Irwin Razaghi <irwin@razaghi.com.au>
2018-09-08 14:10:05 +1000

    Adding basic frame elements

---
+++ src/containers/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react';
+import Shell from './Shell/Shell';
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+      <Shell/>
+      </Container>
+    );
+  }
+}
+
+export default App;
