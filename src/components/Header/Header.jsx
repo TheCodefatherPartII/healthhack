5bc5fc9d882f8c9820a7b04892e3d7c29350f355
Irwin Razaghi <irwin@razaghi.com.au>
2018-09-08 14:52:27 +1000

    Search handling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,18 @@ import {
 } from 'semantic-ui-react';
 
 class Header extends React.Component {
+  state = { searchString: '' };
+
+  onChange = (i, event) => {
+    this.setState({ searchString: event.value.trim() });
+  };
+
+  onKeyPress = event => {
+    if (event.key.toLowerCase() !== 'enter') return;
+
+    console.log('perform search callback');
+    // this.props.performSearch(this.state.searchString);
+  };
 
   render() {
     return (
@@ -15,7 +27,14 @@ class Header extends React.Component {
         </Menu.Item>
         <Menu.Menu position="right" fluid style={{width: "100%"}}>
           <Menu.Item fluid style={{width: "100%"}}>
-            <Input icon='search' placeholder='Search...' />
+            <Input
+
+              value={this.state.searchString}
+              onChange={this.onChange}
+              onKeyPress={this.onKeyPress}
+              icon='search'
+              placeholder='Search...'
+            />
           </Menu.Item>
         </Menu.Menu>
       </Menu>
