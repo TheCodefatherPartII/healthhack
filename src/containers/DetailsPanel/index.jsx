b4b23ce8362e93c54e31546448555ba53665429c
Mahesh Chauhan <mahesh.chauhan@terem.com.au>
2018-09-09 13:33:30 +1000

    Crime bar graph

--- src/containers/DetailsPanel/index.jsx
+++ src/containers/DetailsPanel/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import DataService  from '../../services/DataService';
-
+import {Bar} from '@nivo/bar';
 import {
   Input,
   Header,
@@ -20,6 +20,7 @@ import {
   Grid
 } from "semantic-ui-react";
 import AboutPane from "../../components/About";
+
 
 class DetailsPanel extends React.Component {
   state = { };
@@ -45,9 +46,37 @@ class DetailsPanel extends React.Component {
     if (lga)
       this.setState({ lga });
   }
+
+  getChartData(lgacrime) {
+    const chartData = lgacrime.map((lga) => {
+      return {
+        "lgaId": lga.lgaId,
+        "murder": lga.murder,
+        "murderColor": "hsl(327, 70%, 50%)",
+        "domesticViolence": lga.domesticViolence,
+        "domesticViolenceColor": "hsl(138, 70%, 50%)",
+        "nonDomesticViolence": lga.nonDomesticViolence,
+        "nonDomesticViolenceColor": "hsl(128, 70%, 50%)",
+        "sexualAssault" : lga.sexualAssault,
+        "sexualAssaultColor": "hsl(120, 100%, 79%)",
+        "robbery": lga.robbery,
+        "robberyColor": "hsl(145, 100%, 79%)",
+        "breakIns" : lga.breakIns,
+        "breakInsColor": "hsl(189, 100%, 79%)",
+        "fraud" : lga.fraud,
+        "fraudColor": "hsl(250, 100%, 79%)",
+        "motorVehicleTheft" : lga.motorVehicleTheft,
+        "motorVehicleTheftColor": "hsl(220, 100%, 79%)",
+      }
+    })
+    console.log("chartData : " + chartData)
+    return chartData
+  }
+  
   renderStatistics = () => {
     console.log(this.state.lga);
-
+    const crime = this.props.crime
+    const chartData = this.getChartData(crime).slice(1,15)
     const population = parseInt(this.state.lga.POPULATION, 10);
     return (
       <div>
@@ -80,6 +109,114 @@ class DetailsPanel extends React.Component {
               <ListItem key={ind}>{ind}</ListItem>
             ))}
           </List>
+        </Segment>
+        <Segment >
+        <Bar height={500} width={800}
+        data={chartData}
+        keys={[
+            "murder",
+            "domesticViolence",
+            "nonDomesticViolence",
+            "sexualAssault",
+            "robbery",
+            "breakIns",
+            "fraud",
+            "motorVehicleTheft"
+        ]}
+        indexBy="lgaId"
+        margin={{
+            "top": 50,
+            "right": 130,
+            "bottom": 50,
+            "left": 60
+        }}
+        padding={0.3}
+        colors="nivo"
+        colorBy="id"
+        defs={[
+            {
+                "id": "dots",
+                "type": "patternDots",
+                "background": "inherit",
+                "color": "#38bcb2",
+                "size": 4,
+                "padding": 1,
+                "stagger": true
+            },
+            {
+                "id": "lines",
+                "type": "patternLines",
+                "background": "inherit",
+                "color": "#eed312",
+                "rotation": -45,
+                "lineWidth": 6,
+                "spacing": 10
+            }
+        ]}
+        fill={[
+            {
+                "match": {
+                    "id": "fries"
+                },
+                "id": "dots"
+            },
+            {
+                "match": {
+                    "id": "sandwich"
+                },
+                "id": "lines"
+            }
+        ]}
+        borderColor="inherit:darker(1.6)"
+        axisBottom={{
+            "orient": "top",
+            "tickSize": 5,
+            "tickPadding": 5,
+            "tickRotation": 0,
+            "legend": "Local government area",
+            "legendPosition": "middle",
+            "legendOffset": 36
+        }}
+        axisLeft={{
+            "orient": "left",
+            "tickSize": 5,
+            "tickPadding": 5,
+            "tickRotation": 0,
+            "legend": "Crimes",
+            "legendPosition": "middle",
+            "legendOffset": -40
+        }}
+        labelSkipWidth={12}
+        labelSkipHeight={12}
+        labelTextColor="inherit:darker(1.6)"
+        animate={true}
+        motionStiffness={90}
+        motionDamping={15}
+        legends={[
+            {
+                "dataFrom": "keys",
+                "anchor": "bottom-right",
+                "direction": "column",
+                "justify": false,
+                "translateX": 120,
+                "translateY": 0,
+                "itemsSpacing": 2,
+                "itemWidth": 100,
+                "itemHeight": 20,
+                "itemDirection": "left-to-right",
+                "itemOpacity": 0.85,
+                "symbolSize": 20,
+                "effects": [
+                    {
+                        "on": "hover",
+                        "style": {
+                            "itemOpacity": 1
+                        }
+                    }
+                ]
+            }
+        ]}
+    />
         </Segment>
       </div>
     );
@@ -272,7 +409,7 @@ class DetailsPanel extends React.Component {
     if (!this.state.lga) return <AboutPane/>;
 
     const panes = [
-      { menuItem: "Information", render: this.renderStatistics },
+      { menuItem: "Information", render: this.renderStatistics.bind(this) },
       { menuItem: "Contacts", render: this.renderContactDetails },
       { menuItem: "Petitions", render: this.renderPetitionsDetails }
     ];
