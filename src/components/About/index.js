a8bf8fbd5343d06c9ce1dbbc0a1108d28f0a8bc7
Irwin Razaghi <irwin@razaghi.com.au>
2018-09-09 15:43:38 +1000

    A few changes

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -19,13 +19,23 @@ class AboutPane extends React.PureComponent {
         />
         <Segment attached>
           <Header as='h4'>How does this work?</Header>
+          To start using the application click on the map on the left to zoom into your Local Government Area to reveal a wide variety of statistics.
           <Divider/>
           <List as='ul'>
-            <List.Item as='li'>To start using the application click on the map on the left to zoom into your Local Government Area to reveal a wide variety of statistics.</List.Item>
-            <List.Item as='li'>Informations pane: This pane shows the aggregated data about the LGA; like a census data, number of services in the area and a quick comparison snapshot against other surrounding councils, etc.</List.Item>
-            <List.Item as='li'>Contacts Pane: This pane shares information about the local Council Representative. </List.Item>
-            <List.Item as='li'>Petitions Pane: This pane allows to start a petition on a concerning issue in the community. Community members can view all the available petitions and vote on them.</List.Item>
-            <List.Item as='li'>Statistics Pane: This pane shows you the statistics on community services like Health Services, Schools, Child Care Centers, etc and lets you compare the Crime rate with other LGAs.</List.Item>
+            <List.Item as='li'><strong>Information Pane:</strong>
+              <br/>This pane shows the aggregated data about the LGA; like a census data, number of services in the area and a quick comparison snapshot against other surrounding councils, etc.
+              <br/><br/>
+            </List.Item>
+            <List.Item as='li'><strong>Contacts Pane:</strong><br/>
+              This pane shares information about the local Council Representative.<br/><br/>
+            </List.Item>
+            <List.Item as='li'>
+              <strong>Petitions Pane:</strong>
+              <br/>This pane allows to start a petition on a concerning issue in the community. Community members can view all the available petitions and vote on them.
+              <br/><br/>
+            </List.Item>
+            <List.Item as='li'><strong>Statistics Pane:</strong><br/>
+              This pane shows you the statistics on community services like Health Services, Schools, Child Care Centers, etc and lets you compare the Crime rate with other LGAs.</List.Item>
           </List>
         </Segment>
       </div>
